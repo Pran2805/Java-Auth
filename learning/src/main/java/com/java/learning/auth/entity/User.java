b168329492a42a5f5b41e0dceaed1707befc31ea
Pran2805 <pranavshinde.as@gmail.com>
2026-03-06 21:21:51 +0530

    feat: user entity design

---
+++ learning/src/main/java/com/java/learning/auth/entity/User.java
@@ -0,0 +1,42 @@
+package com.java.learning.auth.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private LocalDate lastLogin;
+
+    private boolean isVerified;
+
+    private String resetPasswordToken;
+    private String verifyToken;
+    private LocalDate resetPasswordExpiresAt;
+    private LocalDate verificationTokenExpiresAt;
+
+
+}
